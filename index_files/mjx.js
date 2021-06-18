137f92018f8862de95ed5719b0a09187c347d9e1
XiangYu Wang <oxq@me.com>
2021-06-19 05:02:55 +0800

    new

---
+++ index_files/mjx.js
@@ -0,0 +1,20 @@
+ window.MathJax={ svg:{ fontCache: 'global' } ,
+ tex:{ inlineMath: [['$', '$'], ['\\(', '\\)']] } };
+ (function () { var script = document.createElement('script');
+ script.src= 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js';
+ script.async= true; document.head.appendChild(script); })();
+
+ //===================================================================
+ // To load Mathjax, set a file with the above commands and link it in
+ // your page by adding in its header a line, like the following one:
+ //       <script src=".w/mjx.js" async></script> </head><body>
+ // Note: Latex formulas below require Java Scripts enabled for this page,
+ // for https://cdn.jsdelivr.net and https://www.cs.bu.edu/fac/lnd/mjx.js
+ //===================================================================
+
+ //=======================================================================
+ // <script> MathJax={ svg:{ fontCache: 'global' } ,
+ // tex:{ inlineMath: [['$', '$'], ['\\(', '\\)']] } };
+ // </script> <script type="text/javascript" async
+ // src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js"> </script>
+ //=======================================================================
